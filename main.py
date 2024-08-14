9002f92399c0d037c08bcb0c948246b21136256e
Juan Segundo Arnaude <juansarnaude@gmail.com>
2024-08-14 12:35:04 -0300

    possible movement??

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from models.Sokoban import Sokoban
 with open(f"{sys.argv[1]}", "r") as file:
     sokoban = read_sokoban_level(file)
     sokoban.move('u')
+    sokoban.move('d')
     sokoban.move('r')
     sokoban.display_map()
 
--- models/Sokoban.py
+++ models/Sokoban.py
@@ -31,22 +31,37 @@ class Sokoban:
     #VERTICAL COORDINATES ARE OPPOSITE OF WHAT WE USUALLY USE SO
     #UP IS +(0,-1), DOWN IS +(1,0)
     def move(self, direction):
+        #UP
         if direction == 'u':
+            if self.map[self.player.y][self.player.x-1] == '#':
+                return
             self.map[self.player.y][self.player.x] = ' '
             self.player.move(0, -1)
             self.map[self.player.y][self.player.x] = '@'
 
+
+        #DOWN
         elif direction == 'd':
+            if self.map[self.player.y][self.player.x-1] == '#':
+                return
             self.map[self.player.y][self.player.x] = ' '
             self.player.move(0, 1)
             self.map[self.player.y][self.player.x] = '@'
 
+
+        #RIGHT
         elif direction == 'r':
+            if self.map[self.player.y+1][self.player.x] == '#':
+                return
             self.map[self.player.y][self.player.x] = ' '
             self.player.move(1, 0)
             self.map[self.player.y][self.player.x] = '@'
 
+
+        #LEFT
         elif direction == 'l':
+            if self.map[self.player.y-1][self.player.x] == '#':
+                return
             self.map[self.player.y][self.player.x] = ' '
             self.player.move(-1, 0)
             self.map[self.player.y][self.player.x] = '@'
