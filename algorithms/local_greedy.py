8c7904bd2c0e41c30517ba02922fc750d7c2c2ec
Juan Segundo Arnaude <juansarnaude@gmail.com>
2024-08-20 19:00:10 -0300

    local_greedy improved a bit but still doesnt work

--- algorithms/local_greedy.py
+++ algorithms/local_greedy.py
@@ -6,40 +6,42 @@ from models.Node import Node
 
 def local_greedy(initial_state, map, heuristics):
     explored = set()
-
     current_node = Node(initial_state).set_cost(0)
-    root_node=current_node
+    root_node = current_node
     heuristics.apply(current_node)
 
-    while current_node:
+    explored.add(current_node.state)
 
+    while current_node:
         if current_node.state.is_goal_state(map):
+            print("OOOSSSAAA")
+            print(current_node.cost)
+            print(len(explored))
             return current_node, len(explored), 0
 
-        explored.add(current_node.state)
+
 
         min_cost = float('inf')
         best_node = None
-        min_state = None
 
         for direction in Direction:
             child_state = current_node.state.move(direction, map)
             if child_state and child_state not in explored:
-                new_node = Node(child_state, current_node, direction, cost=0)
-
+                new_node = Node(child_state, current_node, direction, cost=current_node.cost + 1)
                 heuristics.apply(new_node)
 
+                # Compare the cost of the next states locally
                 if new_node.cost < min_cost:
-                    min_state = child_state
                     min_cost = new_node.cost
                     best_node = new_node
 
+        # Check if best node exist
         if best_node:
             current_node = best_node
-            explored.add(min_state)
-        elif current_node==root_node:
-            return None
+            explored.add(current_node.state)
         else:
-            #backtracking
-            current_node = current_node.parent
+            #Backtrack
+            if current_node!=root_node:
+                current_node = current_node.parent
 
+    return None
--- heuristics/boxes_in_goals.py
+++ heuristics/boxes_in_goals.py
@@ -3,8 +3,9 @@
 def boxes_in_goals(node = None, map = None):
     if not hasattr(boxes_in_goals, "map"):
         boxes_in_goals.map = map
+        boxes_in_goals.n_boxes = len(map.boxes)
         return
     else:
-        node.add_cost(- len(node.state.boxes.intersection(boxes_in_goals.map.goals)))
+        node.add_cost(- len(node.state.boxes.intersection(boxes_in_goals.map.goals)) / boxes_in_goals.n_boxes)
         return
 
